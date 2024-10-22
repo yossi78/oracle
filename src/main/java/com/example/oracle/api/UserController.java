5e027eeeecc9e796a21ec331242635ea34a33c42
yossi78 <yossi78@gmail.com>
2024-10-22 15:39:58 +0300

    fix controller

--- src/main/java/com/example/oracle/api/UserController.java
+++ src/main/java/com/example/oracle/api/UserController.java
@@ -4,11 +4,11 @@ import com.example.oracle.repository.User;
 import com.example.oracle.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-
 import java.util.List;
+
+
 
 @RestController
 @RequestMapping("/api/users")
